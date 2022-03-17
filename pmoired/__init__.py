0a3a5d44d4192c25281d345fa30905fc0f305f51
Antoine Mérand <antoine.merand@gmail.com>
2022-03-17 11:39:15 +0100

    tweaking kepler disk model

--- pmoired/__init__.py
+++ pmoired/__init__.py
@@ -29,7 +29,7 @@ import requests
 print('[P]arametric [M]odeling of [O]ptical [I]nte[r]ferom[e]tric [D]ata', end=' ')
 print('https://github.com/amerand/PMOIRED')
 
-__version__='20220314'
+__version__='20220317'
 
 __versions__={'pmoired':__version__,
               'python':sys.version,
