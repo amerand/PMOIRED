3a8c028b5991db5fd4d12756de6a875970c0daf1
Antoine Mérand <antoine.merand@gmail.com>
2024-03-28 07:48:50 +0100

    version 1.3 without examples

--- pmoired/__init__.py
+++ pmoired/__init__.py
@@ -30,7 +30,7 @@ MAX_THREADS = multiprocessing.cpu_count()
 #print('[P]arametric [M]odeling of [O]ptical [I]nte[r]ferom[e]tric [D]ata', end=' ')
 #print('https://github.com/amerand/PMOIRED')
 
-__version__= '1.0.20230720'
+__version__= '1.3'
 
 __versions__={'pmoired':__version__,
               'python':sys.version,
